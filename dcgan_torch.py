7a608443e816940b64819633ce78358b648239ca
EunWoo Im <imeunukr@gmail.com>
2021-08-13 11:42:05 +0900

    Update dcgan_torch.py

--- dcgan_torch.py
+++ dcgan_torch.py
@@ -1,3 +1,4 @@
+import os
 import random
 
 import torch
@@ -12,13 +13,12 @@ import torchvision.transforms as transforms
 def trainset(params):
     transform = transforms.Compose([
         transforms.ToTensor(),
-        transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5))
-        ])
+        transforms.Normalize([0.5], [0.5])])
     dataset = torchvision.datasets.MNIST(
         root=params['root'],train=True,download=True,transform=transform
         )
     trainloader = torch.utils.data.DataLoader(
-        dataset, batch_size=params['batch_size'],shuffle=True, 
+        dataset, batch_size=params['batch_size'],shuffle=True,
         num_workers=params['workers']
         )
     cuda_avble = torch.cuda.is_available()
@@ -37,21 +37,10 @@ def weights_init(m):
 class Generator(nn.Module):
     def __init__(self, params):
         super(Generator, self).__init__()
-        # self.ngpu = ngpu
         self.gen = nn.Sequential(
-            nn.ConvTranspose2d(params['nz'], params['ngf'] * 8, 4, 1, 0, bias=False),
-            nn.BatchNorm2d(params['ngf'] * 8),
-            nn.ReLU(True),
-            nn.ConvTranspose2d(params['ngf'] * 8, params['ngf'] * 4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(params['ngf'] * 4),
-            nn.ReLU(True),
-            nn.ConvTranspose2d( params['ngf'] * 4, params['ngf'] * 2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(params['ngf'] * 2),
-            nn.ReLU(True),
-            nn.ConvTranspose2d( params['ngf'] * 2, params['ngf'], 4, 2, 1, bias=False),
-            nn.BatchNorm2d(params['ngf']),
-            nn.ReLU(True),
-            nn.ConvTranspose2d( params['ngf'], params['nc'], 4, 2, 1, bias=False),
+            nn.ConvTranspose2d(params['nz'], params['ngf']*2, 5, 2, 0, bias=False),
+            nn.ConvTranspose2d(params['ngf']*2,params['ngf'], 5, 2, 0, bias=False),
+            nn.ConvTranspose2d(params['ngf'], params['nc'], 4, 2, 0, bias=False),
             nn.Tanh())
 
     def forward(self, x):
@@ -60,29 +49,21 @@ class Generator(nn.Module):
 class Discriminator(nn.Module):
     def __init__(self, params):
         super(Discriminator, self).__init__()
-        # self.ngpu = ngpu
-        self.main = nn.Sequential(
-            nn.Conv2d(params['nc'], params['ndf'], 4, 2, 1, bias=False),
+        self.discriminate = nn.Sequential(
+            nn.Conv2d(params['nc'], params['ndf'], 5, 2, 4, bias=False),
             nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(params['ndf'], params['ndf'] * 2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(params['ndf'] * 2),
+            nn.Conv2d(params['ndf'], params['ndf'] * 2, 5, 2, 4, bias=False),
             nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(params['ndf'] * 2, params['ndf'] * 4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(params['ndf'] * 4),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(params['ndf'] * 4, params['ndf'] * 8, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(params['ndf'] * 8),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(params['ndf'] * 8, 1, 4, 1, 0, bias=False),
-            nn.Sigmoid()
-        )
+            nn.Flatten(),
+            nn.Linear(10 * 10 * params['ndf'] * 2, 1),
+            nn.Sigmoid())
 
     def forward(self, input):
-        return self.main(input)
+        return self.discriminate(input)
 
 if __name__ == '__main__':
     params = {
-    'root' : 'C:\Users\eunwoo\Desktop\Code\savehere',
+    'root' : '/content/mnist',
     'cuda' : True,
     'batch_size' : 128,
     'workers' : 2,
@@ -101,17 +82,22 @@ if __name__ == '__main__':
     random.seed(params['seed'])
     torch.manual_seed(params['seed'])
 
+    try:
+        os.mkdir(params['root'])
+    except:
+        pass
     trainloader, device = trainset(params)
-    generator = Generator().to(device)
+
+    generator = Generator(params).to(device)
     generator.apply(weights_init)
-    discriminator = Discriminator().to(device)
+    discriminator = Discriminator(params).to(device)
     discriminator.apply(weights_init)
 
     criterion = nn.BCELoss()
     noise = torch.randn(params['visualize'],params['nz'],1,1,device=device)
 
-    real_label = 1
-    fake_label = 0
+    real_label = 1.0
+    fake_label = 0.0
 
     optimizerD = optim.Adam(discriminator.parameters(),
                             lr=params['lr'], betas=(params['beta1'], 0.999))
@@ -123,17 +109,18 @@ if __name__ == '__main__':
     D_losses = []
 
     for epoch in range(params['epochs']):
-        for i, data in enumerate(trainloader):
+        for i, data in enumerate(trainloader,0):
             real_data = data[0].to(device)
             b_size = real_data.size(0)
             label = torch.full((b_size,),real_label,device=device)
             output = discriminator(real_data).view(-1)
+
             errD_real = criterion(output, label)
             errD_real.backward()
             D_x = output.mean().item()
             
             noise = torch.randn(b_size, params['nz'], 1, 1, device=device)
-            fake_data = discriminator(noise)
+            fake_data = generator(noise)
             label.fill_(fake_label)
 
             output = discriminator(fake_data.detach()).view(-1)
@@ -153,9 +140,8 @@ if __name__ == '__main__':
             D_G_z2 = output.mean().item()
             optimizerG.step()
 
-            print(torch.cuda.is_available())
             print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(x): %.4f\tD(G(z)): %.4f / %.4f'
-                % (epoch, params['nepochs'], i, len(trainloader),
+                % (epoch, params['epochs'], i, len(trainloader),
                     errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
 
             D_losses.append(errD.item())
